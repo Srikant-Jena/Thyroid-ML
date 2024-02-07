9ba333e43e2550cb4130b36cd114b30e947a5742
srikant <srikantddazzler387@gmail.com>
2024-02-07 22:23:34 +0530

    first commit

---
+++ static/JS/script.js
@@ -0,0 +1,35 @@
+// TSH
+function toggleTSHVisibility() {
+    var element = document.getElementById('ifTSHTrue');
+    var checkbox = document.getElementById('TrueTSHCheck');
+    element.style.visibility = checkbox.checked ? 'visible' : 'hidden';
+  }
+  
+  // T3
+  function toggleT3Visibility() {
+    var element = document.getElementById('ifT3True');
+    var checkbox = document.getElementById('TrueT3Check');
+    element.style.visibility = checkbox.checked ? 'visible' : 'hidden';
+  }
+  
+  // TT4
+  function toggleTT4Visibility() {
+    var element = document.getElementById('ifTT4True');
+    var checkbox = document.getElementById('TrueTT4Check');
+    element.style.visibility = checkbox.checked ? 'visible' : 'hidden';
+  }
+  
+  // T4U
+  function toggleT4UVisibility() {
+    var element = document.getElementById('ifT4UTrue');
+    var checkbox = document.getElementById('TrueT4UCheck');
+    element.style.visibility = checkbox.checked ? 'visible' : 'hidden';
+  }
+  
+  // FTI
+  function toggleFTIVisibility() {
+    var element = document.getElementById('ifFTITrue');
+    var checkbox = document.getElementById('TrueFTICheck');
+    element.style.visibility = checkbox.checked ? 'visible' : 'hidden';
+  }
+  
